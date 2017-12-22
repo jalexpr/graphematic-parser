e634bad13aa30729217e9b260095177f8d4f38db
jalexpr <32515097+jalexpr@users.noreply.github.com>
2017-12-22 16:09:30 +0300

    Update Parser.java

--- src/parserstring/Parser.java
+++ src/parserstring/Parser.java
@@ -1,106 +1,78 @@
+package parserstring;
 
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map.Entry;
-import org.junit.After;
-import static org.junit.Assert.assertEquals;
-import org.junit.Before;
-import org.junit.Test;
-import parserstring.Parser;
 
-public class TestParserBasicsPhase {
+public class Parser {
 
-    HashMap<String, String[]> parserBasicsPhase = new HashMap<>();
-    HashMap<String, String[][]> parserSentence = new HashMap<>();
-    HashMap<String, String[][][]> parserParagrap = new HashMap<>();
+    public static List<String> parserBasicsPhase(String basicsPhase) throws RuntimeException {
 
-    @Before
-    public void setParserBasicsPhase() {
-        parserBasicsPhase.put("Привет как дела", new String[]{"Привет", "как", "дела"});
-        parserBasicsPhase.put("Привет как дела!", new String[]{"Привет", "как", "дела"});
-        parserBasicsPhase.put("Привет как дела! ", new String[]{"Привет", "как", "дела"});
-        parserBasicsPhase.put(" Привет как дела! ", new String[]{"Привет", "как", "дела"});
-        parserBasicsPhase.put("Привет как дела!!!!&", new String[]{"Привет", "как", "дела"});
-        parserBasicsPhase.put("Привет как дела,&, как жизнь", new String[]{"Привет", "как", "дела", "как", "жизнь"});
+        basicsPhase = basicsPhase.replaceAll("[^а-я^А-Я^-^0-9]", " ");
 
-        parserSentence.put("Привет как дела", new String[][]{{"Привет", "как", "дела"}});
-        parserSentence.put("Привет как дела!", new String[][]{{"Привет", "как", "дела"}});
-        parserSentence.put("Привет как дела!!!!", new String[][]{{"Привет", "как", "дела"}});
-        parserSentence.put("Привет как дела, как жизнь?", new String[][]{{"Привет", "как", "дела"}, {"как", "жизнь"}});
-        parserSentence.put("Привет как дела, как жизнь????", new String[][]{{"Привет", "как", "дела"}, {"как", "жизнь"}});
-        parserSentence.put("Привет как дела, как жизнь????&", new String[][]{{"Привет", "как", "дела"}, {"как", "жизнь"}});
-        parserSentence.put(" Привет как дела,,,, как жизнь?! Чем обычно занимаешь? ", new String[][]{{"Привет", "как", "дела"}, {"как", "жизнь"}, {"Чем", "обычно", "занимаешь"}});
+        LinkedList<String> strList = new LinkedList<>();
 
-        parserParagrap.put("Привет как дела", new String[][][]{{{"Привет", "как", "дела"}}});
-        parserParagrap.put("Привет как дела!", new String[][][]{{{"Привет", "как", "дела"}}});
-        parserParagrap.put("Привет как дела!!!!", new String[][][]{{{"Привет", "как", "дела"}}});
-        parserParagrap.put("Привет как дела, как жизнь?", new String[][][]{{{"Привет", "как", "дела"}, {"как", "жизнь"}}});
-        parserParagrap.put("Привет как дела, как жизнь????", new String[][][]{{{"Привет", "как", "дела"}, {"как", "жизнь"}}});
-        parserParagrap.put("Привет как дела, как жизнь????&", new String[][][]{{{"Привет", "как", "дела"}, {"как", "жизнь"}}});
-        parserParagrap.put(" Привет как дела,,,, как жизнь?! Чем обычно занимаешь?  Привет как дела,,,, как жизнь?! Чем обычно занимаешь?", new String[][][]{{{"Привет", "как", "дела"}, {"как", "жизнь"}}, {{"Чем", "обычно", "занимаешь"}}, {{"Привет", "как", "дела"}, {"как", "жизнь"}}, {{"Чем", "обычно", "занимаешь"}}});
-        parserParagrap.put(" Привет как дела,,,, как жизнь?! Чем обычно занимаешь?  Привет как дела,,,, как жизнь?! Чем обычно занимаешь? ", new String[][][]{{{"Привет", "как", "дела"}, {"как", "жизнь"}}, {{"Чем", "обычно", "занимаешь"}}, {{"Привет", "как", "дела"}, {"как", "жизнь"}}, {{"Чем", "обычно", "занимаешь"}}});
-    }
-
-    @After
-    public void tear() {
-        parserBasicsPhase.clear();
-        parserSentence.clear();
-    }
-
-    @Test
-    public void testParserBasicsPhase() {
-        parserBasicsPhase.entrySet().forEach((entry) -> {
-            final String[] expected = entry.getValue();
-            final String testData = entry.getKey();
-            final List<String> actual = Parser.parserBasicsPhase(testData);
-            testParserBasicsPhase(actual, expected);
-        });
-    }
-
-    public void testParserBasicsPhase(List<String> actual, String[] expected) {
-        assertEquals(expected.length, actual.size());
-        int index = 0;
-        for (String word : actual) {
-            assertEquals(expected[index], word);
-            index++;
-        };
-    }
-
-    @Test
-    public void testParserSentence() {
-        parserSentence.entrySet().forEach((entry) -> {
-            final String[][] expected = entry.getValue();
-            final String testData = entry.getKey();
-            final List<List<String>> actual = Parser.parserSentence(testData);
-
-        });
-    }
-
-    public void testParserSentence(List<List<String>> actual, String[][] expected) {
-        assertEquals(expected.length, actual.size());
-        int indexX = 0;
-        for (List<String> basicsPhase : actual) {
-            testParserBasicsPhase(basicsPhase, expected[indexX]);
-            indexX++;
-        };
-    }
-
-    @Test
-    public void testParserParagraph() {
-        parserParagrap.entrySet().forEach((entry) -> {
-            final String[][][] expected = entry.getValue();
-            final String testData = entry.getKey();
-            final List<List<List<String>>> actual = Parser.parserParagraph(testData);
-            testParserParagraph(actual, expected);
-        });
-    }
-    
-    public void testParserParagraph(List<List<List<String>>> actual, String[][][] expected) {
-        assertEquals(expected.length, actual.size());
-        int indexX = 0;
-        for (List<List<String>> sentence : actual) {
-            testParserSentence(sentence, expected[indexX]);
-            indexX++;
+        for (String str : basicsPhase.split(" ")) {
+            if (!str.equals("")) {
+                strList.add(str);
+            }
         }
+
+        if (strList.isEmpty()) {
+            throw new RuntimeException("Передана пустая строка");
+        }
+        return strList;
     }
+
+    public static List<List<String>> parserSentence(String sentence) throws RuntimeException {
+
+        sentence = sentence.replaceAll("[@\"№#;$%^:&*()\\!?.]", ",");
+
+        List<List<String>> sentenceList = new LinkedList<>();
+
+        for (String basicsPhase : sentence.split(",")) {
+            if (!basicsPhase.equals("")) {
+                try {
+                    sentenceList.add(parserBasicsPhase(basicsPhase));
+                } catch (RuntimeException ex) {
+                }
+            }
+        }
+
+        if (sentenceList.isEmpty()) {
+            throw new RuntimeException("Передана пустая строка");
+        }
+
+        return sentenceList;
+    }
+
+    public static List<List<List<String>>> parserParagraph(String paragraph) {
+
+        List<List<List<String>>> paragraphList = new LinkedList<>();
+
+        for (String sentence : paragraph.split("[@\"№#;$%^:&*()\\!?.]")) {
+            if (!sentence.equals("")) {
+                try {
+                    paragraphList.add(parserSentence(sentence));
+                } catch (RuntimeException ex) {
+
+                }
+            }
+        }
+
+        return paragraphList;
+    }
+
+    public static List<List<List<List<String>>>> parserText(String text) {
+
+        List<List<List<List<String>>>> textList = new LinkedList<>();
+
+        for (String paragraph : text.split("[\\r\\n]")) {
+            if (!paragraph.equals("")) {
+                textList.add(parserParagraph(paragraph));
+            }
+        }
+
+        return textList;
+    }
+
 }
