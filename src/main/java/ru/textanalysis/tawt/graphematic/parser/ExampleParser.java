514588b3a50718067137239a97ee9d56dc273b17
jalexpr <alex.porechny@mail.ru>
2019-10-15 22:40:47 +0300

    0.0.2 graphematic-parser

--- src/main/java/ru/textanalysis/tawt/graphematic/parser/ExampleParser.java
+++ src/main/java/ru/textanalysis/tawt/graphematic/parser/ExampleParser.java
@@ -6,28 +6,23 @@ import ru.textanalysis.tawt.graphematic.parser.text.GraphematicParser;
 
 import java.util.List;
 
+
 public class ExampleParser {
-    public static void main(String...args) throws NotParserTextException {
-        String sentence;
-        if (args == null || args.length == 0) {
-            sentence = "Осенний марафон: стало ясно, " +
-                    "что будет с российской валюто. Справедливый курс, по мнению " +
-                    "аналитиков, — на уровне 65-66.";
-        } else {
-            sentence = args[0];
+    public static void main(String... args) throws NotParserTextException {
+        {
+            GraphematicParser parser = new GParserImpl();
+            List<List<String>> listBasicPhase = parser.parserSentence("Parser - это программа"
+                    + " начального анализа естественного текста, представленного в виде цепочки"
+                    + " символов, вырабатывающая информацию, необходимую для дальнейшей обработки.");
+            System.out.println(listBasicPhase);
         }
 
-        GraphematicParser parser = new GParserImpl();
-        List<List<String>> listBasicPhase = parser.parserSentence("Parser - это программа"
-        + " начального анализа естественного текста, представленного в виде цепочки"
-        + " символов, вырабатывающая информацию, необходимую для дальнейшей обработки.");
-        System.out.println(listBasicPhase);
-
-
-
-        List<List<List<String>>> listBasicsPhase = parser.parserParagraph("Осенний марафон -"
-                + " стало ясно, что будет с российской валютой. Справедливый курс,"
-                + " по мнению аналитиков, — на уровне 65-66.");
-        System.out.println(listBasicsPhase);
+        {
+            GraphematicParser parser = new GParserImpl();
+            List<List<List<String>>> listBasicsPhase = parser.parserParagraph("Осенний марафон -"
+                    + " стало ясно, что будет с российской валютой. Справедливый курс,"
+                    + " по мнению аналитиков, — на уровне 65-66.");
+            System.out.println(listBasicsPhase);
+        }
     }
 }
